ed5fe6246364a88b23a2ee2651fb01c69335764f
Dillon Dugan <dugan_dillon@yahoo.com>
2020-05-01 21:48:31 -0700

    lets save this point

---
+++ content/pages/about/about.md
@@ -0,0 +1,16 @@
+---
+title: 'About the Project'
+slug: about-the-project
+cover: ./theCar.png
+disqus: true
+---
+
+## The Electric Corvette
+
+I have always been a tinkerer. I have always enjoyed a project. I am concerned about our over reliance on fossil fuels and the impact of that on the world we leave behind when were gone. I love corvettes. I think that is why I have found my self at this project.
+
+### The mission of this project is to make a performant and decently ranged electric vehicle out of my 1978 Chevrolet Corvette
+
+### The Car
+
+The car is a 1978 Chevrolet Corvette former pace car. It came stock with a small-block 350 with a 8.2:1 compression ratio. It has a torque rating of 280 lb-ft @ 2400 rpm and a curb weight of 3,529 lb.
---
+++ data/siteConfig.js
@@ -0,0 +1,74 @@
+module.exports = {
+  siteTitle: 'The Electric Corvette',
+  siteDescription: 'A journey in EV conversion.',
+  authorName: 'Dillon Dugan',
+  twitterUsername: '_clumsychemist',
+  authorAvatar: 'avatar.jpg', // file in content/images
+  defaultLang: 'en', // show flag if lang is not default. Leave empty to enable flags in post lists
+  authorDescription: `Dillon Dugan is a Software Engineer from Chandler, AZ, and a lifelong sustainability enthusiast.`,
+  siteUrl: 'https://www.electricvette.blog/',
+  disqusSiteUrl: 'https://www.electricvette.blog/',
+  // Prefixes all links.
+  pathPrefix: '/electric', // Note: it must *not* have a trailing slash.
+  siteCover: 'theCar2.png', // file in content/images
+  googleAnalyticsId: 'UA-67868977-1',
+  background_color: '#ffffff',
+  theme_color: '#222222',
+  display: 'standalone',
+  icon: '',
+  postsPerPage: 6,
+  disqusShortname: 'Dillon',
+  headerTitle: 'The Electric Corvette',
+  headerLinksIcon: '', //  (leave empty to disable: '')
+  headerLinks: [
+    {
+      label: 'Blog',
+      url: '/',
+    },
+    {
+      label: 'About',
+      url: '/about-the-project',
+    },
+    // {
+    //   label: 'Installation',
+    //   url: '/how-to-install',
+    // },
+  ],
+  // Footer information (ex: Github, Netlify...)
+  websiteHost: {
+    name: 'Netlify',
+    url: 'https://www.netlify.com/',
+  },
+  footerLinks: [
+    {
+      sectionName: 'Explore',
+      links: [
+        {
+          label: 'Blog',
+          url: '/',
+        },
+        {
+          label: 'About',
+          url: '/about-the-project',
+        },
+        // {
+        //   label: 'Installation',
+        //   url: '/how-to-install',
+        // },
+      ],
+    },
+    {
+      sectionName: 'Follow the author',
+      links: [
+        // {
+        //   label: 'Website',
+        //   url: 'https://www.maxpou.fr',
+        // },
+        {
+          label: 'Twitter',
+          url: 'https://twitter.com/_clumsychemist',
+        },
+      ],
+    },
+  ],
+}
